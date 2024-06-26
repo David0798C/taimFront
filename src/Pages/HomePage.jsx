0b7ca5c83c05cde94ff58f135f0b7c40fd7dab5d
CarmenAmez <carmensasamez@gmail.com>
2024-06-26 19:50:33 +0200

    fondo fijo, añadido divs descripción y añadidas imagnes de prueba de fondo.

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,68 +1,43 @@
-
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import Logo from '../pic/Ilustración_con_título.png';
-
-// Estilos para los enlaces
-const Enlace = styled(Link)`
-  text-decoration: none;
-  color: black;
-  display: block;
-  padding: 10px 20px;
-`;
-
-// Estilos para los botones
-const Button = styled.button`
-  margin: 10px;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-  border: none;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #f0f0f0;
-  }
-`;
-
-// Estilos para la imagen
-const Img = styled.img`
-  width: 30%;
-  height: auto;
-  display: block;
-`;
-
-// Contenedor para centrar los elementos
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 100vh;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  width: 100%;
-`;
+import { Enlace, Button, Img, Container, ButtonContainer, Background, DescriptionContainer, Description } from '../StyledComponents/StyledHomePages';
 
 const HomePage = () => {
   return (
-    <Container>
-      <Img src={Logo} alt="" />
-      <ButtonContainer>
-        <Button>
-          <Enlace to="/login">Inicio Sesión</Enlace>
-        </Button>
-        <Button>
-          <Enlace to="/register">Registrarse</Enlace>
-        </Button>
-      </ButtonContainer>
-    </Container>
+    <>
+      <Background />
+      <Container>
+        <Img src={Logo} alt="Logo" />
+        <ButtonContainer>
+          <Button>
+            <Enlace to="/login">Inicio Sesión</Enlace>
+          </Button>
+          <Button>
+            <Enlace to="/register">Registrarse</Enlace>
+          </Button>
+        </ButtonContainer>
+        <DescriptionContainer>
+          <Description>
+            <h1>¿Qué es TAIM?</h1>
+            <p>Taim es una web de intercambio de servicios en el cual la moneda de cambio es el tiempo. <br /> Somos los poco conocidos Bancos del Tiempo.</p>
+          </Description>
+          <Description>
+            <h1>¿Qué ofrecemos?</h1>
+            <p>En esta web encontrarás a personas que ofrecen sus habilidades y conocimientos a cambio de las tuyas.</p>
+          </Description>
+                  <Description>
+          <h1>¿Cómo funciona?</h1>
+          <p>
+            Lo primero de todo será registrarse en la página. <br />
+            Luego tendrás que rellenar tu perfil con las habilidades y conocimientos que tienes para ofrecer y lo que te gustaría recibir a cambio de ellas. <br />
+            A continuación tendrás que añadir lo más valioso: <strong>Tu tiempo</strong> . <br />
+            Una vez todo esto añadido podrás esperar que alguien quiera hacer un trato contigo o <br />
+            entrar en la sección de Ofertas y ver qué cosas hay que te interesen y hacer tú los tratos.
+          </p>
+        </Description>
+        </DescriptionContainer>
+
+      </Container>
+    </>
   );
 };
 
---
+++ src/StyledComponents/StyledHomePages.js
@@ -0,0 +1,78 @@
+// HomePageStyles.js
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import BackgroundImage from '../pic/fondo1.jpg'
+
+export const Enlace = styled(Link)`
+    text-decoration: none;
+    color: black;
+    display: block;
+    padding: 10px 20px;
+`;
+
+export const Button = styled.button`
+    margin: 10px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border: none;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    background-color: #fff;
+    transition: background-color 0.3s;
+
+    &:hover {
+    background-color: #f0f0f0;
+}
+`;
+
+export const Img = styled.img`
+    width: 40%;
+    height: auto;
+    display: block;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+  position: relative; // Añadir posición relativa para que los hijos puedan usar posiciones absolutas
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+`;
+
+export const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${BackgroundImage});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    opacity: 0.6; 
+  z-index: -1; // Asegúrate de que el fondo esté detrás de los otros elementos
+`;
+
+export const DescriptionContainer = styled.div`
+
+    margin-top: 20px;
+    font-size: 1.2em;
+`;
+
+export const Description = styled.div`
+    margin: 10px;
+    padding: 10px;
+    background: rgba(255, 255, 255, 0.8);
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    text-align: center;
+    max-width: 1200px;
+`;
