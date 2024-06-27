beb54fd03acaa1f024210e515a05cecfe711232c
CarmenAmez <carmensasamez@gmail.com>
2024-06-27 16:21:14 +0200

    primera prueba

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,5 +1,19 @@
-const UserPage = () => {
-  return <div>UserPage</div>;
+import { Container,CardContainer, Button } from '../StyledComponents/StyledUserPage';
+
+const UserProfile = () => {
+  return (
+    <Container>
+      <CardContainer>
+        <img src="https://placehold.co/300?text=User" alt="User Profile" className="profile-image" />
+        <div>
+          <h1 className="name">John Doe</h1>
+          <p className="role">Web Developer</p>
+          <Button>Follow</Button>
+        </div>
+      </CardContainer>
+    </Container>
+  );
 };
 
-export default UserPage;
+export default UserProfile;
+
