6a5d1918442f0e4ff8d1887829c7690a8013d519
David0798C <dacama777@gmail.com>
2024-06-27 16:18:15 +0200

    offers styled components

--- src/Pages/Ofertas.jsx
+++ src/Pages/Ofertas.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import {
+  ContainerOfertas,
+  ContainerRow,
+  ContainerColumn,
+  H3,
+} from "../StyledComponents/StyledOfertes";
 
 const Ofertas = () => {
-  return <div>Ofertas</div>;
+  return (
+    <div>
+      <ContainerOfertas>
+        <h1>Ofertas</h1>
+
+        <ContainerRow>
+          <ContainerColumn>
+            <H3>Nombre de la oferta</H3>
+          </ContainerColumn>
+        </ContainerRow>
+      </ContainerOfertas>
+    </div>
+  );
 };
 
 export default Ofertas;
---
+++ src/StyledComponents/StyledOfertes.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ContainerOfertas = styled.div``;
+
+export const ContainerRow = styled.div``;
+
+export const ContainerColumn = styled.div``;
+
+export const h3 = styled.h3``;
