7fa223ebcb6d085388e0dd9654f8ad5882bf389f
hausofanna <annacanivell97@gmail.com>
2024-07-02 18:10:38 +0200

    maquetación user page

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,47 +1,81 @@
 import {
-  Container,
-  CardContainer,
-  Button,
-  InterestContainer,
-  Background,
+    Container,
+    CardContainer,
+    Button,
+    InterestContainer,
+    Background,
+    SkillsContainer,
+    ButtonEdit,
 } from "../styledComponents/StyledUserPage";
-const UserProfile = () => {
-  return (
-    <>
-      <Background />
-      <Container>
-        <CardContainer>
-          <img
-            src="https://placehold.co/300?text=User"
-            alt="User Profile"
-            className="profile-image"
-          />
+import { useEffect, useState } from 'react';
+import { getUser } from "../services/user";
 
-          <div>
-            <h1 className="name">John</h1>
-            <p>Web Developer</p>
-            <Button>Follow</Button>
-          </div>
-        </CardContainer>
-        <InterestContainer>
-          <h1>Lo que ofrezco</h1>
-          <br />
-          <ul>
-            <li>Matemáticas</li>
-            <li>Química</li>
-          </ul>
-        </InterestContainer>
-        <InterestContainer>
-          <h1>Lo que busco</h1>
-          <br />
-          <ul>
-            <li>Latín</li>
-            <li>Historia del Arte</li>
-          </ul>
-        </InterestContainer>
-      </Container>
-    </>
-  );
+const UserProfile = () => {
+    const [data, setData] = useState();
+    const [name, setName] = useState();
+    const [surname, setSurname] = useState();
+    const [email, setEmail] = useState();
+    const [userName, setUserName] = useState();
+    const [birthday, setBirthday] = useState();
+    const [profilePic, setProfilePic] = useState();
+    const [location, setLocation] = useState();
+    const [description, setDescription] = useState();
+    const [interests, setInterests] = useState();
+    const [skills, setSkills] = useState();
+    const [rating, setRating] = useState();
+
+    useEffect(() => {
+        getUser().then(data => setData(data.data))
+    }, []);
+
+    return (
+        <>
+            <Background />
+            <Container>
+                <CardContainer>
+                    <ButtonEdit>Editar</ButtonEdit>
+                    <img
+                        src="https://placehold.co/300?text=User"
+                        alt="User Profile"
+                        className="profile-image"
+                    />
+                    <div>
+                        <h1 className="name">Nombre Apellido </h1>
+                        <p>@username</p>
+                        <p>@email</p>
+                        <p>Localidad</p>
+                        <p>Desarrollador Web</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, minima. Eos eum nisi nam atque veritatis culpa mollitia minima perferendis.</p>
+                        <Button>Seguir</Button>
+                    </div>
+                </CardContainer>
+                <SkillsContainer>
+                    <h1>Habilidades</h1>
+                    <ul>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Node.js</li>
+                    </ul>
+                </SkillsContainer>
+                <InterestContainer>
+                    <h1>Lo que ofrezco</h1>
+                    <br />
+                    <ul>
+                        <li>Matemáticas</li>
+                        <li>Química</li>
+                    </ul>
+                </InterestContainer>
+                <InterestContainer>
+                    <h1>Lo que busco</h1>
+                    <br />
+                    <ul>
+                        <li>Latín</li>
+                        <li>Historia del Arte</li>
+                    </ul>
+                </InterestContainer>
+            </Container>
+        </>
+    );
 };
 
 export default UserProfile;
