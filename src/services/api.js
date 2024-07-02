b2e3b2f86bcb535efead3f8d977e3b841ecc7381
Erixxon75 <supervilla77@gmail.com>
2024-07-02 19:04:26 +0200

    login con variable global, oculta enseña segun logeado

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,6 @@ import axios from 'axios';
 export const instance = axios.create ({baseURL: 'http://localhost:8080'});
 
 
+export const setAuth = (username, password) => {
+    instance.defaults.headers.common.Authorization = 'Basic ' + btoa(username + ':' + password);;
+};
