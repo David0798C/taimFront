1c81338777746df2501876864d281388fff6d500
CarmenAmez <carmensasamez@gmail.com>
2024-07-04 19:20:49 +0200

    cambio de UserProvider al main

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import Router from "./Router";
-import { useState, React } from "react";
-import UserProvider from "./providers/UserProvider";
+import UserProvider, { useUserContext } from "./providers/UserProvider";
+import Login from "./pages/Login";
 
 function App() {
+  const [user] = useUserContext();
 
-  return (
-    <div>
-      <UserProvider>
-        <Router />
-      </UserProvider>
-    </div>
-  );
+  console.log('1111111111111', user)
+  return user ? <Router /> : <Login />;
+
 }
 
 export default App;
+
