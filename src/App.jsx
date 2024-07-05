2f9a506a0edce1ee1be985b7fdd524d35d42d91a
Erixxon75 <supervilla77@gmail.com>
2024-07-05 18:29:51 +0200

    Local storage hecho y funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import Router from "./Router";
-import RouterUL from "./RouterUL";
 import { React } from "react";
 import {useUserContext} from "./providers/UserProvider";
 
 function App() {
 
-  const [user] = useUserContext();
-
   return (
     <div>
-      user ? <Router /> : <RouterUL />;
+     <Router/>
     </div>
   );
 }
