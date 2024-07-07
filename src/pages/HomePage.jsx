d3d4394370bedb0841bb3b46b4c0e51f9d1c7a0b
CarmenAmez <carmensasamez@gmail.com>
2024-07-07 14:59:00 +0200

    instalado aos y usando en HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,11 +9,19 @@ import {
   GlobalStyle,
 } from "../styledComponents/StyledHomePages.js";
 
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { MyContext } from "../MyContext.js";
 import 'animate.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Importa los estilos CSS de AOS
 
 const HomePage = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Duración de la animación en milisegundos
+      once: true, // La animación ocurre solo una vez
+    });
+  }, []);
 
   const { logged, setLogged } = useContext(MyContext);
 
@@ -23,45 +31,42 @@ const HomePage = () => {
       <GlobalStyle />
       <H1 className="animate__animated animate__slideInLeft">T A I M</H1>
 
-      {" "}
       {!logged && (
-        
         <ButtonContainer>
           <Button>
-            <Enlace to="/login">Inicio Sesión</Enlace>
+            <Enlace href="/login">Inicio Sesión</Enlace>
           </Button>
           <Button>
-            <Enlace to="/register">Registrarse</Enlace>
+            <Enlace href="/register">Registrarse</Enlace>
           </Button>
         </ButtonContainer>
       )}
+
       <Container>
-
-
         <DescriptionContainer>
-          <Description>
+          <Description data-aos="fade-up">
             <h1>¿Qué es TAIM?</h1>
             <p>
-              TAIM, Tiempo Acumulado de Intercamvio Mutuo, <br /> es una web de
+              TAIM, Tiempo Acumulado de Intercambio Mutuo, <br /> es una web de
               intercambio de servicios en el cual la moneda de cambio es el
               tiempo. <br /> Somos los poco conocidos Bancos del Tiempo.
             </p>
           </Description>
-          <Description>
+          <Description data-aos="fade-up">
             <h1>¿Qué ofrecemos?</h1>
             <p>
               En esta web encontrarás a personas que ofrecen sus habilidades y
               conocimientos a cambio de las tuyas.
             </p>
           </Description>
-          <Description>
+          <Description data-aos="fade-up">
             <h1>¿Cómo funciona?</h1>
             <p>
               Lo primero de todo será registrarse en la página. <br />
               Luego tendrás que rellenar tu perfil con las habilidades y
               conocimientos que tienes para ofrecer y lo que te gustaría recibir
               a cambio de ellas. <br />A continuación tendrás que añadir lo más
-              valioso: <strong>Tu tiempo</strong> . <br />
+              valioso: <strong>Tu tiempo</strong>. <br />
               Una vez todo esto añadido podrás esperar que alguien quiera hacer
               un trato contigo o <br />
               entrar en la sección de Ofertas y ver qué cosas hay que te
