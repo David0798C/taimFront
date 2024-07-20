b035961f70f1962ac9c5f482efdd60f37c5219b1
David0798C <dacama777@gmail.com>
2024-07-20 12:52:32 +0200

    intento de hacer lo de las request

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,13 +5,48 @@ import {
   InterestContainer,
   SkillsContainer,
   LogoutButtonContainer,
+  TaskContainer,
+  TaskText,
+  TaskButton,
+  H1,
+  Task,
 } from "../styledComponents/StyledUserPage";
 import { RiEdit2Fill } from "react-icons/ri";
 import { useUserContext } from "../providers/UserProvider";
 import { GlobalStyle, Enlace } from "../styledComponents/StyledHomePages.js";
+import { useEffect, useState } from "react";
+import { getRequest } from "../services/request.js";
 
 const UserProfile = () => {
   const [user, , logout] = useUserContext();
+
+  const [request, setRequest] = useState();
+
+  //PARA CONSEGUIR LAS ID DE LAS REQUEST DE LAS TAREAS DEL USUARIO QUE HA INICIADO SESSION
+  const idsRequestsUserTasks = user.task.map((task) => {
+    const requestIds = task.request.map((id) => {
+      return id.id;
+    });
+    return requestIds;
+  });
+
+  //PETICION A LAS REQUEST
+  useEffect(() => {
+    getRequest().then((res) => {
+      setRequest(res.data);
+    });
+  }, []);
+
+  //CLG DE LAS REQUEST ENTERAS
+  console.log("111111", request);
+
+  //LO DE LAS IDS DE LAS REQUEST DE LAS TAREAS DEL USUARIO
+  console.log("3333333333333", idsRequestsUserTasks);
+
+  //FILTER QUE NO FUNCIONA PARA INTENTAR CONSEGUIR LAS ID DE TODAS LAS REQUEST DE LA BASE DE DATOS Y LUEGO COMPARARLAS CON LAS (idsRequestsUserTasks) PARA PODER GUARDAR LA ID DEL USUARIO QUE LA HA ENVIADO Y MOSTRAR LOS DATOS DEL USUARIO
+  const userId = request.filter((requestId) => requestId.id);
+
+  console.log(userId, "222222222222222");
 
   return (
     <>
@@ -50,6 +85,14 @@ const UserProfile = () => {
             <li>{user?.interests}</li>
           </ul>
         </InterestContainer>
+
+        <TaskContainer>
+          <H1>Tareas</H1>
+          <Task>
+            <TaskText>asdas</TaskText>
+            <TaskButton></TaskButton>
+          </Task>
+        </TaskContainer>
       </Container>
     </>
   );
