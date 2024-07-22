6b79dd2f5a43233cbe63416412d46062b9c8f157
hausofanna <annacanivell97@gmail.com>
2024-07-22 15:43:55 +0200

    cambio api.js de localhost a vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const instance = axios.create ({baseURL: 'http://localhost:8080'});
+export const instance = axios.create({ baseURL: 'https://taim-ruby.vercel.app/' });
 
 export const setAuth = (username, password) => {
 
